61502be3772c9e5841edab8ed60024b0f0bb4b2e
Matteo Santelmo <matteo.santelmo@gmail.com>
2023-03-12 16:58:37 +0100

    #1 Defined CommonGoalCard and related classes, without detailed logic specification

--- src/main/java/org/myshelfie/model/CommonGoalDeck.java
+++ src/main/java/org/myshelfie/model/CommonGoalDeck.java
@@ -1,4 +1,17 @@
 package org.myshelfie.model;
 
+import java.util.Collections;
+import java.util.LinkedList;
+
 public class CommonGoalDeck {
+    private LinkedList<CommonGoalCard> deck;
+    public CommonGoalDeck() {
+        /*
+         * Initialize the deck list so that it contains all CommonGoalCards
+         */
+    }
+    public CommonGoalCard drawCommonGoalCard() {
+        Collections.shuffle(deck);
+        return deck.pop();
+    }
 }
