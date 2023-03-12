642793084ec1be071f99e220c1974bc76323353e
Matteo Santelmo <matteo.santelmo@gmail.com>
2023-03-12 16:58:37 +0100

    #2 Defined CommonGoalCard and related classes, without detailed logic…

--- src/main/java/org/myshelfie/model/CommonGoalCard.java
+++ src/main/java/org/myshelfie/model/CommonGoalCard.java
@@ -1,4 +1,23 @@
 package org.myshelfie.model;
 
+import java.util.ArrayDeque;
+
+
 public abstract class CommonGoalCard {
+    private ArrayDeque<ScoringToken> tokens;
+    private String ID;
+
+    public abstract Boolean checkGoalSatisfied();
+    public String getID() {
+        return ID;
+    }
+    public ScoringToken getTopScoringToken() {
+        /*
+         *  exposes the top ScoringToken of the stack represented by tokens
+         */
+        return tokens.peek();
+    }
+    public ScoringToken popTopScoringToken() {
+            return tokens.pop();
+    }
 }
--- src/main/java/org/myshelfie/model/ConcreteCommonCard.java
+++ src/main/java/org/myshelfie/model/ConcreteCommonCard.java
@@ -1,4 +1,16 @@
 package org.myshelfie.model;
 
 public class ConcreteCommonCard extends CommonGoalCard{
+    public ConcreteCommonCard(Integer numPlayer){
+        /*
+         * Initialize the points stack according to number of players
+         */
+    }
+    public Boolean checkGoalSatisfied(){
+        /*
+         * Specific logic that checks if a particular common goal is satisfied
+         */
+        return null;
+    }
+
 }
