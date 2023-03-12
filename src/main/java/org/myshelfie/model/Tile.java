ee367c2552e40cb7dfdfc057c0797c287bad961f
Mattia Vicenzotto <mattia.vicenzotto@mail.polimi.it>
2023-03-12 22:51:35 +0100

    basic implementation of Tile, TileBag, ItemType, Board

--- src/main/java/org/myshelfie/model/Tile.java
+++ src/main/java/org/myshelfie/model/Tile.java
@@ -1,4 +1,14 @@
 package org.myshelfie.model;
 
 public class Tile {
+    private ItemType itemType;
+
+
+    public Tile(ItemType itemType) {
+        this.itemType = itemType;
+    }
+
+    public ItemType getItemType() {
+        return itemType;
+    }
 }
