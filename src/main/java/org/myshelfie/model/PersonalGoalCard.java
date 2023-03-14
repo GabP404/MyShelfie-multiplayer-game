4b8155654ea80673842771655ae7f7c9c3bb7efc
Giuseppe Steduto <34663336+giuseppe-steduto@users.noreply.github.com>
2023-03-14 13:40:41 +0100

    Make PersonalGoalCard immutable

--- src/main/java/org/myshelfie/model/PersonalGoalCard.java
+++ src/main/java/org/myshelfie/model/PersonalGoalCard.java
@@ -1,21 +1,14 @@
 package org.myshelfie.model;
 
-final class PersonalGoalCard {
+public class PersonalGoalCard {
     private Tile[][] constraints;
 
-    public PersonalGoalCard() {
+    public PersonalGoalCard(Tile[][] constraint_tiles) {
         constraints = new Tile[6][5];
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 5; j++) {
-                constraints[i][j] = null;
+                constraints[i][j] = constraint_tiles[i][j];
             }
         }
-    }
-
-    public void setTile(int col, int row, ItemType type) throws TileInsertionException {
-        if (col < 0 || row < 0 || col > 5 || row > 6)
-            throw new TileInsertionException("Coordinates for the tile not valid!");
-
-        constraints[row][col] = new Tile(); //TODO use constructor to specify ItemType
     }
 }
