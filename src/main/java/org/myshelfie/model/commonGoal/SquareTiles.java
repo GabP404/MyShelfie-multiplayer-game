1f21a79834c229baff6fa0a6d3263380dcae6675
Mattia Vicenzotto <mattia.vicenzotto@mail.polimi.it>
2023-03-19 20:11:02 +0100

    implemented SquareTiles: Two groups each containing 4 tiles of                          the same type in a 2x2 square. The tiles                          of one square can be different from                          those of the other square.

---
+++ src/main/java/org/myshelfie/model/commonGoal/SquareTiles.java
@@ -0,0 +1,89 @@
+package org.myshelfie.model.commonGoal;
+
+import org.myshelfie.model.*;
+
+import java.util.ArrayDeque;
+
+/*
+    Two groups each containing 4 tiles of
+    the same type in a 2x2 square. The tiles
+    of one square can be different from
+    those of the other square.
+ */
+
+public class SquareTiles extends CommonGoalCard {
+    /**
+     * Initialize the CommonGoalCard associating the points' stack to it
+     *
+     * @param id
+     * @param tokens The token stack that will be placed on the card
+     *               NOTE: the stack's generation logic will be in the controller
+     */
+    public SquareTiles(String id, ArrayDeque<ScoringToken> tokens) {
+        super(id, tokens);
+    }
+
+    @Override
+    public Boolean checkGoalSatisfied(Bookshelf bookshelf) {
+
+        boolean[][] visited = new boolean[Bookshelf.NUMROWS][Bookshelf.NUMCOLUMNS];
+        //number of squares to be found
+        int numGroups = 2;
+        //counter for the squares
+        int numGroupsFound = 0;
+        Tile tmp = null;
+
+        for (int i = 0; i < Bookshelf.NUMROWS - 1; i++) {
+            for (int j = 0; j < Bookshelf.NUMCOLUMNS - 1; j++) {
+                if (!visited[i][j]) {
+                    int tmpGroupSize = 0;
+                    try {
+                        tmp = bookshelf.getTile(i, j);
+                        try {
+                            if (tmp != null) {
+                                tmpGroupSize = getGroupSize(visited, i, j, tmp.getItemType(), bookshelf);
+                            }
+                        } catch (TileUnreachableException e) {
+                            // unhandled exception
+                        }
+                    } catch (TileUnreachableException e) {
+                        // unhandled exception
+                    }
+                    //if the group has 4 tiles analyze their shape
+                    if (tmpGroupSize == 4) {
+                        try {
+                            if (bookshelf.getTile(i, j + 1).getItemType() == tmp.getItemType() &&
+                                    bookshelf.getTile(i + 1, j).getItemType() == tmp.getItemType() &&
+                                    bookshelf.getTile(i + 1, j + 1).getItemType() == tmp.getItemType()) {
+                                numGroupsFound++;
+                            }
+                        } catch (TileUnreachableException e) {
+                            // unhandled exception
+                        }
+                    }
+                    if (numGroupsFound >= numGroups) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+
+    }
+
+    //code from (SameTypeGroupings), there might be a way to not duplicate the code. TO BE DISCUSSED
+    private int getGroupSize(boolean[][] visited, int row, int col, ItemType targetType, Bookshelf b) throws TileUnreachableException {
+        if (row < 0 || row >= Bookshelf.NUMROWS || col < 0 || col >= Bookshelf.NUMCOLUMNS || visited[row][col] || b.getTile(row, col) == null || b.getTile(row, col).getItemType() != targetType) {
+            return 0;
+        }
+        visited[row][col] = true;
+        int size = 1;
+        size += getGroupSize(visited, row - 1, col, targetType, b); // check above
+        size += getGroupSize(visited, row + 1, col, targetType, b); // check below
+        size += getGroupSize(visited, row, col - 1, targetType, b); // check left
+        size += getGroupSize(visited, row, col + 1, targetType, b); // check right
+        return size;
+    }
+
+
+}
