da0ecad69a7f67c15ce6bfea349f9a378d1b1479
Matteo Santelmo <matteo.santelmo@gmail.com>
2023-04-02 20:07:55 +0200

    Defined CLI View

---
+++ src/main/java/org/myshelfie/view/CommandLineInterface.java
@@ -0,0 +1,19 @@
+package org.myshelfie.view;
+
+import org.myshelfie.model.Choice;
+import org.myshelfie.model.Game;
+import org.myshelfie.model.GameView;
+import org.myshelfie.util.Observable;
+
+public class CommandLineInterface extends Observable<Choice> implements Runnable {
+
+
+    @Override
+    public void run() {
+
+    }
+
+    public void update(GameView o, Game.Event arg) {
+
+    }
+}
