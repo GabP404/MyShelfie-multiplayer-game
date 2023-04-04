2233bcf78db3ff5085333a8863905641b3b88b14
Matteo Santelmo <matteo.santelmo@gmail.com>
2023-04-04 15:10:08 +0200

    Defined information stored in the view to allow interaction with user.

--- src/main/java/org/myshelfie/view/CommandLineInterface.java
+++ src/main/java/org/myshelfie/view/CommandLineInterface.java
@@ -2,11 +2,29 @@ package org.myshelfie.view;
 
 import org.myshelfie.model.Game;
 import org.myshelfie.model.GameView;
+import org.myshelfie.model.LocatedTile;
 import org.myshelfie.network.CommandMessageType;
 import org.myshelfie.util.Observable;
 
-public class CommandLineInterface extends Observable<CommandMessageType> implements Runnable {
+import java.util.List;
 
+public class CommandLineInterface extends Observable<CommandMessageType> implements Runnable {
+    private final String nickname;
+    private List<LocatedTile> selectedTiles;    // tiles selected from the board
+    private int selectedColumn;     // the column into insert a new hand of tiles
+    private int tileIndex;          // during tiles insertion, the index inside the hand of the tile to insert
+    private String chatMessage;
+    private String whispNickname;
+    // TODO: add attribute for chat message log
+
+    public CommandLineInterface(String nickname) {
+        this.nickname = nickname;
+        this.selectedTiles = null;
+        this.selectedColumn = -1;
+        this.tileIndex = -1;
+        this.chatMessage = null;
+        this.whispNickname = null;
+    }
 
     @Override
     public void run() {
@@ -16,4 +34,5 @@ public class CommandLineInterface extends Observable<CommandMessageType> impleme
     public void update(GameView o, Game.Event arg) {
 
     }
+
 }
