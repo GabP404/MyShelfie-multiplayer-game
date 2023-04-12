2eb53920f09565ad266cd420210cd992ba6c3cfb
Giuseppe Steduto <giuseppe@steduto.it>
2023-04-12 23:01:56 +0200

    global: move variables to JSON configuration file
    The number of each common goal card is not included in the JSON Configuration file as it would be too inconvenient for now. We may think about adding it in the future.

--- src/test/java/org/myshelfie/model/BookshelfTest.java
+++ src/test/java/org/myshelfie/model/BookshelfTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Bookshelf")
-class BookshelfTest {
+public class BookshelfTest {
     private Bookshelf shelf;
 
     @BeforeEach
