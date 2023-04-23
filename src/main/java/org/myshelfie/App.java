b460f16cba84cb560233248e64b9610908881650
Mattia Vicenzotto <mattia.vicenzotto@mail.polimi.it>
2023-04-23 15:49:41 +0200

    CLI graphic implementation (not ready / TESTING)

--- src/main/java/org/myshelfie/App.java
+++ src/main/java/org/myshelfie/App.java
@@ -1,24 +1,11 @@
 package org.myshelfie;
 
-import org.myshelfie.model.*;
-import org.myshelfie.network.client.Client;
-import org.myshelfie.network.client.ClientImpl;
-import org.myshelfie.network.server.Server;
-import org.myshelfie.network.server.ServerImpl;
 import org.myshelfie.view.CommandLineInterface;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-
 public class App {
-
     public static void main( String[] args ) {
 
-        TestCLI textUi = new TestCLI();
+        CommandLineInterface textUi = new CommandLineInterface("loremIpsum");
         textUi.run();
     }
 }
