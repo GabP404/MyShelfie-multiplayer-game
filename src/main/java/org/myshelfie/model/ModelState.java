cc3cc128271d42b47b2a3706440303beeccdd650
Gabriele Puglisi <gabriele.puglisi@mail.polimi.it>
2023-05-03 21:53:21 +0200

    Removed Created Game status and adding methods to GameController.java in order to handle the creation of the Game from the first player

--- src/main/java/org/myshelfie/model/ModelState.java
+++ src/main/java/org/myshelfie/model/ModelState.java
@@ -2,7 +2,6 @@ package org.myshelfie.model;
 
 public enum ModelState {
 
-    CREATED_GAME,
     WAITING_SELECTION_TILE,
     WAITING_3_SELECTION_TILE_FROM_HAND,
     WAITING_2_SELECTION_TILE_FROM_HAND,
