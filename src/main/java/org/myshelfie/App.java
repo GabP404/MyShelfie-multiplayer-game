47f31b817355488ec37cee76d9490011f1cd5b3b
Mattia Vicenzotto <mattia.vicenzotto@mail.polimi.it>
2023-05-06 10:54:16 +0200

    Graphic thread implementation (server to client works)

--- src/main/java/org/myshelfie/App.java
+++ src/main/java/org/myshelfie/App.java
@@ -1,11 +1,24 @@
 package org.myshelfie;
 
+import org.myshelfie.model.*;
+import org.myshelfie.network.client.Client;
+import org.myshelfie.network.client.ClientImpl;
+import org.myshelfie.network.server.Server;
+import org.myshelfie.network.server.ServerImpl;
 import org.myshelfie.view.CommandLineInterface;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
 public class App {
+
     public static void main( String[] args ) {
 
-        CommandLineInterface textUi = new CommandLineInterface("loremIpsum");
+        TestCLI textUi = new TestCLI();
         textUi.run();
     }
 }
