720f839dac85b71f25300b5ac1209ebe3d389698
Gabriele Puglisi <gabriele.puglisi@mail.polimi.it>
2023-05-06 17:38:15 +0200

    refactor from list to hashMap to handle GameController.java inside LobbyController.java

--- src/main/java/org/myshelfie/controller/LobbyController.java
+++ src/main/java/org/myshelfie/controller/LobbyController.java
@@ -7,6 +7,7 @@ import org.myshelfie.model.PersonalGoalDeck;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,10 +15,10 @@ public class LobbyController {
 
     private static LobbyController single_istance;
 
-    private List<GameController> gameControllers;
+    private HashMap<UUID,GameController> gameControllers;
 
     private LobbyController() {
-        gameControllers = new ArrayList<>();
+        gameControllers = new HashMap<>();
     }
 
     public static LobbyController getInstance(){
@@ -27,47 +28,28 @@ public class LobbyController {
         return single_istance;
     }
 
-    public void createNewGame(int size, int rules, String nickname) {
-        GameController gameController = new GameController(UUID.randomUUID(), size, rules);
+    public void createNewGame(int numPlayerGame, int  numGoalCards, String nickname) {
+        UUID uuid = UUID.randomUUID();
+        GameController gameController = new GameController(uuid, numPlayerGame, numGoalCards);
         gameController.addPlayer(nickname);
-        gameControllers.add(gameController);
+        gameControllers.put(uuid,gameController);
     }
 
     public void addPlayerToLobby(String nickname, UUID uuid) throws IOException, URISyntaxException {
-        GameController g = (GameController) gameControllers.stream().filter(x -> x.getGameUuid().equals(uuid));
-        // if (g == null) throws exception
-        g.addPlayer(nickname);
-        if(g.getNicknames().size() == g.getNumPlayerGame()) {
-            try {
-                g.createGame();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            } catch (URISyntaxException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        checkPlayer(g);
+        GameController gameController = gameControllers.get(uuid);
+        gameController.addPlayer(nickname);
     }
 
     //remove player lobby
     public void deleteGame(UUID uuid) {
-        GameController g = (GameController) gameControllers.stream().filter(x -> x.getGameUuid().equals(uuid));
-        gameControllers.remove(g);
+        GameController gameController = gameControllers.get(uuid);
+        gameControllers.remove(gameController);
     }
 
-    private void checkPlayer(GameController g) throws IOException, URISyntaxException {
-        if(g.getNicknames().size() == g.getNumPlayerGame()) {
-            g.createGame();
-        }
-    }
 
     public void removePlayerLobby(String nickname, UUID uuid) {
-        for (GameController g :
-                gameControllers) {
-            if(g.getGameUuid() == uuid && !g.isGameCreated()) {
-                g.getNicknames().remove(nickname);
-            }
-        }
+        GameController gameController = gameControllers.get(uuid);
+        gameController.removePlayer(nickname);
     }
 
 }
