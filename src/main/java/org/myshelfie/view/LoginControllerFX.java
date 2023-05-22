54c01a28869d8e2408f3fabcb71f713b5842e2fa
Gabriele Puglisi <gabriele.puglisi@mail.polimi.it>
2023-05-22 22:06:33 +0200

    Creating Login Scene for JavaFX

---
+++ src/main/java/org/myshelfie/view/LoginControllerFX.java
@@ -0,0 +1,22 @@
+package org.myshelfie.view;
+
+import com.gluonhq.charm.glisten.control.TextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+
+public class LoginControllerFX {
+
+    @FXML
+    private Button JoinGame_BTN;
+
+    @FXML
+    private RadioButton RMI_RB;
+
+    @FXML
+    private RadioButton Socket_RB;
+
+    @FXML
+    private TextField username_LBL;
+
+}
