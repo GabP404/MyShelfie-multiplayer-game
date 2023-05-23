3995dae1cdfeb7be744fd5591a94cc3aabee3108
Gabriele Puglisi <gabriele.puglisi@mail.polimi.it>
2023-05-23 15:08:22 +0200

    Creating: Login Scene,Lobbies Scene,Game Scene and EndGame Scene

---
+++ src/main/java/org/myshelfie/view/LobbiesControllerFX.java
@@ -0,0 +1,22 @@
+package org.myshelfie.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.VBox;
+
+public class LobbiesControllerFX {
+
+    @FXML
+    private Button CreateGame_BTN;
+
+    @FXML
+    private VBox LobbyContainer;
+
+    @FXML
+    private ChoiceBox<?> Players_CB;
+
+    @FXML
+    private ChoiceBox<?> Rules_CB;
+
+}
---
+++ src/main/java/org/myshelfie/view/ViewGUI.java
@@ -0,0 +1,25 @@
+package org.myshelfie.view;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class ViewGUI extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 1920, 1080);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
