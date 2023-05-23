1cb573c985066f6245845d6ca1337487aaf25ecd
Gabriele Puglisi <gabriele.puglisi@mail.polimi.it>
2023-05-23 15:08:22 +0200

    Creating: Login Scene,Lobbies Scene,Game Scene and EndGame Scene

---
+++ src/main/java/org/myshelfie/view/LobbyControllerFX.java
@@ -0,0 +1,18 @@
+package org.myshelfie.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class LobbyControllerFX {
+
+    @FXML
+    private Label GameNameJoin_LB;
+
+    @FXML
+    private Button JoinGame_BTN;
+
+    @FXML
+    private Label NumPlayers_LB;
+
+}
