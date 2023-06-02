3cf02c9cabe3e98448aab577b1bb3a9d760bea2a
Mattia Vicenzotto <mattia.vicenzotto@mail.polimi.it>
2023-06-02 23:06:37 +0200

    added controls in lobby joining/creation

--- src/main/java/org/myshelfie/network/client/Client.java
+++ src/main/java/org/myshelfie/network/client/Client.java
@@ -69,6 +69,7 @@ public class Client extends UnicastRemoteObject implements ClientRMIInterface, R
             } catch (Exception e) {
                 System.err.println("Exception: " + e.getMessage());
                 e.printStackTrace();
+                System.exit(1);
             }
         } else {
             try {
@@ -91,18 +92,8 @@ public class Client extends UnicastRemoteObject implements ClientRMIInterface, R
         view.endNicknameThread();
     }
 
-    public void endCreateGameThread() {
-        view.endCreateGameThread();
-        if (!isRMI) {
-            try {
-                serverListener.start();
-            } catch (java.lang.IllegalThreadStateException e) {
-                // Should never be thrown, but if thrown, then thread was already started!
-            }
-        }
-    }
-    public void endJoinGameThread() {
-        view.endJoinGameThread();
+    public void endChoiceThread() {
+        view.endChoiceThread();
         if (!isRMI) {
             try {
                 serverListener.start();
