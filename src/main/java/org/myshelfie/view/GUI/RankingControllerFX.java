62645180ed108ffb9d291b95a079ec39675dd19a
Gabriele Puglisi <gabriele.puglisi@mail.polimi.it>
2023-06-23 02:20:32 +0200

    created the endGame Scene using two fxml EndGameFXML.fxml and rankingFXML.fxml to build the scene

---
+++ src/main/java/org/myshelfie/view/GUI/RankingControllerFX.java
@@ -0,0 +1,37 @@
+package org.myshelfie.view.GUI;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import org.myshelfie.model.Player;
+import org.myshelfie.model.util.Pair;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RankingControllerFX implements Initializable {
+
+    @FXML
+    private Label points_LB;
+
+    @FXML
+    private Label username_LBL;
+
+    @FXML
+    private ImageView winner_IMG;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    public void setData(Pair<Player,Boolean> result) {
+        Player player = result.getLeft();
+        username_LBL.setText(player.getNickname());
+        points_LB.setText(String.valueOf(player.getTotalPoints()));
+        System.out.println(result.getLeft().getNickname() + result.getRight());
+        if (result.getRight()) {
+            winner_IMG.setVisible(true);
+        }
+    }
+}
