c543eaeedfa701b9e09becb5769e6437d95748fc
Matteo Santelmo <matteo.santelmo@gmail.com>
2023-06-26 10:54:28 +0200

    Docs: added documentation for the classes in the network package regarding listener mechanism and LoggingFormatter. Cleaned the code from warnings and unused code. Addresses #101

--- src/main/java/org/myshelfie/network/server/LoggingFormatter.java
+++ src/main/java/org/myshelfie/network/server/LoggingFormatter.java
@@ -5,6 +5,9 @@ import java.util.Date;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
+/**
+ * Formatter for the server logger
+ */
 public class LoggingFormatter extends Formatter {
     private final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM HH:mm:ss");
 
